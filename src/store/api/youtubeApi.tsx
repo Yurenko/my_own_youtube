1872e376fc57bafd9bdcaed3b8a28df7953abf84
Yurenko <didenkoyura225@gmail.com>
2022-04-03 11:52:39 +0300

    Added API

---
+++ src/store/api/youtubeApi.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+});
+
+export const firstRecomendedVideos = () => {
+  return instance
+    .get(
+      `videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=9&regionCode=UA&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+    )
+    .then((res) => res.data.items);
+};
+
+export const videoCardsAPI = (channelId: string) => {
+  return instance.get(
+    `channels?part=snippet&id=${channelId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+  );
+};
