dcda9c5e1808a38ab8cdb39edd283664a1ad29ca
Yurenko <didenkoyura225@gmail.com>
2022-04-05 17:23:15 +0300

    Updated design SideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -8,6 +8,13 @@ import HistoryIcon from "@material-ui/icons/History";
 import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
 import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import LocalMoviesIcon from "@material-ui/icons/LocalMovies";
+import VideogameAssetIcon from "@material-ui/icons/VideogameAsset";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import SportsIcon from "@material-ui/icons/Sports";
+import SettingsIcon from "@material-ui/icons/Settings";
+import FlagOutlinedIcon from "@material-ui/icons/FlagOutlined";
 
 const SideBar = () => {
   return (
@@ -22,6 +29,14 @@ const SideBar = () => {
       <SideBarRow Icon={WatchLaterIcon} title="Watch later" />
       <SideBarRow Icon={ThumbUpIcon} title="Liked vides" />
       <hr />
+      <p className={styles.subtitle}>MORE FROM YOUTUBE</p>
+      <SideBarRow Icon={YouTubeIcon} title="Youtube Premium" />
+      <SideBarRow Icon={LocalMoviesIcon} title="Movies" />
+      <SideBarRow Icon={VideogameAssetIcon} title="Games" />
+      <SideBarRow Icon={LiveTvIcon} title="Live broadcasts" />
+      <SideBarRow Icon={SportsIcon} title="Sport" />
+      <SideBarRow Icon={SettingsIcon} title="Settings" />
+      <SideBarRow Icon={FlagOutlinedIcon} title="Complaints history" />
     </div>
   );
 };
