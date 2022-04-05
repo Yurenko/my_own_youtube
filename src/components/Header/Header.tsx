bb6dd911587858ddfb3f96f5b4fd8232bf2a0006
Yurenko <didenkoyura225@gmail.com>
2022-04-05 17:21:30 +0300

    Update disign Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,50 @@
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
-import VideoCallIcon from "@material-ui/icons/VideoCall";
+import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
 import AppsIcon from "@material-ui/icons/Apps";
-import NotificationsIcon from "@material-ui/icons/Notifications";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import Avatar from "@material-ui/core/Avatar";
 import styles from "./Header.module.css";
+import { ReactComponent as Youtube } from "../../assets/icon/youtube.svg";
 
 export function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.headerLeft}>
-        <MenuIcon />
-        <img
-          className={styles.headerLogo}
-          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-          alt=""
-        />
+        <MenuIcon style={{ padding: "8px" }} />
+        <div className={styles.headerLogo}>
+          <Youtube />
+        </div>
       </div>
 
       <div className={styles.headerCenter}>
-        <input type="text" className={styles.headerCenterInput} />
-        <SearchIcon className={styles.headerSearchbutton} />
+        <input
+          type="text"
+          className={styles.headerCenterInput}
+          placeholder="Search"
+        />
+        <div className={styles.headerSearchbutton}>
+          <SearchIcon />
+        </div>
       </div>
 
-      <div className={styles.headerRight}>
-        <VideoCallIcon className={styles.headerIcon} />
-        <AppsIcon className={styles.headerIcon} />
-        <NotificationsIcon className={styles.headerIcon} />
-        <Avatar src="/broken-image.jpg" />
-      </div>
+      <ul className={styles.headerRight}>
+        <li className={styles.headerIcon}>
+          <VideoCallOutlinedIcon fontSize="medium" />
+        </li>
+        <li className={styles.headerIcon}>
+          <AppsIcon style={{ width: 24, height: 24 }} />
+        </li>
+        <li className={styles.headerIcon}>
+          <NotificationsOutlinedIcon style={{ width: 24, height: 24 }} />
+        </li>
+        <li>
+          <Avatar
+            src="/broken-image.jpg"
+            style={{ width: "32px", height: "32px" }}
+          />
+        </li>
+      </ul>
     </div>
   );
 }
